6edbfd26360f937205012b49b5eb20f3a0fd8852
Jay <lyl.jay@qq.com>
2025-12-17 21:42:26 +0800

    add 添加分享外链

---
+++ res/linkResources.js
@@ -0,0 +1,24 @@
+const linkResourcesData = [
+    {
+        title: "技术盛宴",
+        list:[
+            {
+                link:"https://learn.lianglianglee.com",
+                description: "技术学习平台"
+            },
+            {
+                link:"https://liaoxuefeng.com/books/java/introduction/index.html",
+                description: "廖雪峰Java教程"
+            }
+        ]
+    },
+    {
+        title: "文档&教程",
+        list: [
+            {
+                link: "https://book.kotlincn.net/text/home.html",
+                description: "Kotlin官方中文文档"
+            }
+        ]
+    }
+]
